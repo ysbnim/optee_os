ysbnim/optee_os

commit bb5d1825919719a1f4c3bc3a19f1040f427d8f60

ta: pkcs11: invalidate handle of destroyed token objects
Marks as invalid any object handle that refers to the token object
that is being destroying so that any use of the related handle will
consider the object handle as invalid.

Link: https://github.com/OP-TEE/optee_os/issues/6005
Reviewed-by: Valerii Chubar <valerii_chubar@epam.com>
Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

ta/pkcs11/src/object.c
@@ -160,6 +160,8 @@ void destroy_object(struct pkcs11_session *session, struct pkcs11_object *obj,
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));
 		cleanup_persistent_object(obj, session->token);
+
+		token_invalidate_object_handles(obj);
 	} else {
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));

ta/pkcs11/src/pkcs11_token.c
@@ -95,6 +95,18 @@ struct pkcs11_session *pkcs11_handle2session(uint32_t handle,
 	return handle_lookup(&client->session_handle_db, handle);
 }
 
+void token_invalidate_object_handles(struct pkcs11_object *obj)
+{
+	struct pkcs11_client *client = NULL;
+	uint32_t handle = 0;
+
+	TAILQ_FOREACH(client, &pkcs11_client_list, link) {
+		handle = handle_lookup_handle(&client->object_handle_db, obj);
+		if (handle)
+			handle_invalidate(&client->object_handle_db, handle);
+	}
+}
+
 struct pkcs11_client *register_client(void)
 {
 	struct pkcs11_client *client = NULL;

ta/pkcs11/src/pkcs11_token.h
@@ -322,6 +322,9 @@ struct ck_token *pkcs11_session2token(struct pkcs11_session *session)
 	return session->token;
 }
 
+/* Invalidate any handle referring the object since the object no more exists */
+void token_invalidate_object_handles(struct pkcs11_object *obj);
+
 /* Entry point for the TA commands */
 enum pkcs11_rc entry_ck_slot_list(uint32_t ptypes, TEE_Param *params);
 enum pkcs11_rc entry_ck_slot_info(uint32_t ptypes, TEE_Param *params);

commit f30cc54aedf9611583d29190e519f0414773d104

ta: pkcs11: invalidate handles before freeing token objects
Move token_invalidate_object_handles() before cleanup_persistent_object()
so that handles are invalidated while obj is still valid.

cleanup_persistent_object() frees obj through cleanup_volatile_obj_ref().
Calling token_invalidate_object_handles() afterwards therefore passes a
pointer to an object whose lifetime has ended.

Fixes: bb5d18259197 ("ta: pkcs11: invalidate handle of destroyed token objects")
Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>

ta/pkcs11/src/object.c
@@ -159,9 +159,9 @@ void destroy_object(struct pkcs11_session *session, struct pkcs11_object *obj,
 
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));
-		cleanup_persistent_object(obj, session->token);
-
 		token_invalidate_object_handles(obj);
+
+		cleanup_persistent_object(obj, session->token);
 	} else {
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));